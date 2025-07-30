3597403a4d9e4f36f85a480289e7396b6f746120
chikyuugaugokidasita <79787986+ROsssay@users.noreply.github.com>
2025-07-30 16:38:39 +0900

    condition_variable
    WINAPI를 이용한 Event 보다는
    condition_variable을 이용한 관리가 더 좋다

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,65 +4,76 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <Windows.h>
 
-class SpinLock
+
+mutex m;
+queue<int32> q;
+HANDLE handle;
+
+// CV는 userLevel Object (커널 오브젝트 x)
+condition_variable cv;
+
+void Producer()
 {
-public:
-	void lock()
+	while (true)
 	{
-		// CAS (Compare And Swap)
-		bool expected = false;
-		bool desired = true;
+		// 1) Lock을 잡고
+		// 2) 공유 변수 값을 수정
+		// 3) Lock을 풀고
+		// 4) 조건변수 통해 다른 쓰레드에게 통지
 
-		while (_locked.compare_exchange_strong(expected, desired) == false) // 처음에 기대하는 값이 expected
-		{																	// 바뀌길 기대하는 값이 desired 딱알았으
-			expected = false;
+		{
+			unique_lock<mutex> lock(m); //1) 3)
+			q.push(100);				//2)
+		}
 
-			//this_thread::sleep_for(std::chrono::milliseconds(100));
-			this_thread::sleep_for(0ms);
-			//this_thread::yield(); == 0ms
+		cv.notify_one(); // wait중인 스레드가 있으면 딱 한개를 깨운다 4)
+
+		//::SetEvent(handle);			// 4)
+		//this_thread::sleep_for(10000ms);
+	}
+}
+
+void Consumer()
+{
+	while (true)
+	{
+		unique_lock<mutex> lock(m);
+		cv.wait(lock, []() {return q.empty() == false; });
+		// 1) Lock을 잡고
+		// 2) 조건 확인
+		// - 만족 => 빠져나와서 이어서 코드를 진행
+		// - 만족x => Lock을 풀어주고 대기상태로 전환
+
+		// 그런데 notify_one을 했으면 항상 조건식을 만족하는거 아닐까?
+		// Spurious Wakeup(가짜 기상?)
+		// notify_one할 때 lock을 잡고 있는 것이 아니기 때문
+
+		// while (q.empty() == false) << 43번줄에서 체크하기때문에 굳이 체크 안해도 됨
+		{
+			int32 data = q.front();
+			q.pop();
+			cout << q.size() << endl;
 		}
 	}
-
-	void unlock()
-	{
-		//_locked = false;
-		_locked.store(false);
-	}
-private:
-	atomic<bool> _locked = false;
-};
-
-SpinLock spinLock;
-mutex m;
-int32 sum = 0;
-
-void Add()
-{
-	for (int32 i = 0; i < 10'0000; i++)
-	{
-		lock_guard<SpinLock> guard(spinLock);
-		sum++;
-	}
 }
 
-void Sub()
-{
-	for (int32 i = 0; i < 10'0000; i++)
-	{
-		lock_guard<SpinLock> guard(spinLock);
-		sum--;
-	}
-}
+// 온라인 게임 서버에서 이용할때
+// 한쪽에서 패킷을 쏘고, 컨텐츠 큐에서 데이터를 꺼내서 사용해야 할때,
+// 효율적으로 리소스를 관리하기 위해서
 
 int main()
 {
+	// 커널 오브젝트
+	// Usage Count
+	// Signal(파란불) / Non-Signal(빨간불) << bool
+	// Auto / Manual << bool
 
-	thread t1(Add);
-	thread t2(Sub);
+
+	thread t1(Producer);
+	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
-
-	cout << sum << endl;
 }
