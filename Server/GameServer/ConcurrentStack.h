2024f286bc936e6545bcd9e3a925f094a6dce8a0
chikyuugaugokidasita <79787986+ROsssay@users.noreply.github.com>
2025-08-14 19:20:59 +0900

    Lock-Free Stack #1

--- Server/GameServer/ConcurrentStack.h
+++ Server/GameServer/ConcurrentStack.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <mutex>
+#include <atomic>
 
 template<typename T>
 
@@ -59,5 +60,80 @@ private:
 	stack<T> _stack;
 	mutex _mutex;
 	condition_variable _condVar;
+};
+
+template<typename T>
+class LockFreeStack
+{
+	struct Node
+	{
+		Node(const T& value) : data(value)
+		{
+
+		}
+
+		T data;
+		Node* next;
+	};
+
+public:
+	
+	// 1) 새 노드를 만들고
+	// 2) 새 노드의 NEXT = HEAD
+	// 3) head = 새 노드
+
+	// [ ][ ][ ][ ][ ][ ][ ][ ]
+	// [head]
+	void Push(const T& value)
+	{
+		Node* node = new Node(value);
+		node->next = _head;
+
+		while (_head.compare_exchange_weak(node->next, node) == false)
+		{
+
+		}
+
+
+		// 이 사이에 새치기 당하면?
+		// _head = node;
+	}
+
+
+	// 1) head 읽기
+	// 2) head->next 읽기
+	// 3) head = head->next
+	// 4) data 추출해서 반환
+	// 5) 추출한 노드 삭제
+
+	// [ ][ ][ ][ ][ ][ ][ ][ ]
+	// [head]
+	bool TryPop(T& value)
+	{
+		Node* oldHead = _head;
+
+		while (oldHead && _head.compare_exchange_weak(oldHead, oldHead->next) == false)
+		{
+
+		}
+
+		if (oldHead == nullptr)
+			return false;
+
+		// Exception X
+		value = oldHead->data;
+
+		// 잠시 삭제 보류
+		//delete oldHead;
+
+		// C#, Java 같이 GC가 있으면 여기서 끝
+
+		return true;
+	}
+
+private:
+	// [ ][ ][ ][ ][ ][ ][ ][ ]
+	// [head]
+	atomic<Node*> _head;
 };
 
